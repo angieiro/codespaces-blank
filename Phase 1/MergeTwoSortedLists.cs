7b0e7061eb7e43a0c08b7153b64c1507d6b00722
Angeliki-Iro Fragkou-Argyriou <iro.fragkou@gmail.com>
2025-09-04 23:46:57 +0000

    Merge Two Sorted Lists

--- Phase 1/MergeTwoSortedLists.cs
+++ Phase 1/MergeTwoSortedLists.cs
@@ -10,7 +10,26 @@ public class ListNode {
 }
 
 public static class MergeListSolution {
-    public static ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode resultListNode = new ListNode(0);
+        ListNode opNode = resultListNode;
+        while (list1 is not null && list2 is not null)
+        {
+                if (list2.val >= list1.val)
+                {
+                    opNode.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    opNode.next = list2;
+                    list2 = list2.next;
+                }
+                opNode = opNode.next;
+        }
+        opNode.next = list1 ?? list2;
+        return resultListNode.next;
         
     }
 }
